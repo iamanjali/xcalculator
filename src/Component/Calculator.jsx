e67df978dc4a5e227efed12d853caf236c7ac7dc
iamanjali <iamanjalijaiswal@gmail.com>
2024-10-20 18:18:34 +0530

    Simple Calculator

--- src/Component/Calculator.jsx
+++ src/Component/Calculator.jsx
@@ -36,26 +36,26 @@ function Calculator() {
   return (
     <>
       <h1>React Calculator</h1>
-      <input value={num} />
+      <input type="text" value={num} />
       <div>{res}</div>
       <br />
-      <button onClick={() => handleInput(7)}>7</button>
-      <button onClick={() => handleInput(8)}>8</button>
-      <button onClick={() => handleInput(9)}>9</button>
+      <button onClick={() => handleInput("7")}>7</button>
+      <button onClick={() => handleInput("8")}>8</button>
+      <button onClick={() => handleInput("9")}>9</button>
       <button onClick={() => handleInput("+")}>+</button>
       <br />
-      <button onClick={() => handleInput(4)}>4</button>
-      <button onClick={() => handleInput(5)}>5</button>
-      <button onClick={() => handleInput(6)}>6</button>
+      <button onClick={() => handleInput("4")}>4</button>
+      <button onClick={() => handleInput("5")}>5</button>
+      <button onClick={() => handleInput("6")}>6</button>
       <button onClick={() => handleInput("-")}>-</button>
       <br />
-      <button onClick={() => handleInput(1)}>1</button>
-      <button onClick={() => handleInput(2)}>2</button>
-      <button onClick={() => handleInput(3)}>3</button>
+      <button onClick={() => handleInput("1")}>1</button>
+      <button onClick={() => handleInput("2")}>2</button>
+      <button onClick={() => handleInput("3")}>3</button>
       <button onClick={() => handleInput("*")}>*</button>
       <br />
       <button onClick={() => handleInput("c")}>c</button>
-      <button onClick={() => handleInput(0)}>0</button>
+      <button onClick={() => handleInput("0")}>0</button>
       <button onClick={() => handleInput("=")}>=</button>
       <button onClick={() => handleInput("/")}>/</button>
     </>
