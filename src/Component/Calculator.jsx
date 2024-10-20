c250a52506607cf02d33e2f60b0cf7a2ef9b407b
iamanjali <iamanjalijaiswal@gmail.com>
2024-10-20 16:52:52 +0530

    Simple Calculator

---
+++ src/Component/Calculator.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./Calculator.css";
+
+function Calculator() {
+  let [num, setNum] = useState("");
+  let [res, setRes] = useState("");
+
+  let handleInput = (val) => {
+    if (val === "c"){
+      setNum("");
+      setRes("");
+    }
+     else if(val === "="){
+      try {
+        if (num === "") {
+          setRes("Error");
+        } else {
+          const result = eval(num);
+          if (result === Infinity) {
+            setRes("Infinity");
+          } else if (isNaN(result)) {
+            setRes("NaN");
+          } else {
+            setRes(result);
+          }
+        }
+      } catch {
+        setRes("Error");
+      }
+     }
+    else {
+        setNum((prev) => prev+""+val)
+    };
+  };
+
+  return (
+    <>
+      <h1>React Calculator</h1>
+      <input value={num} />
+      <div>{res}</div>
+      <br />
+      <button onClick={() => handleInput(7)}>7</button>
+      <button onClick={() => handleInput(8)}>8</button>
+      <button onClick={() => handleInput(9)}>9</button>
+      <button onClick={() => handleInput("+")}>+</button>
+      <br />
+      <button onClick={() => handleInput(4)}>4</button>
+      <button onClick={() => handleInput(5)}>5</button>
+      <button onClick={() => handleInput(6)}>6</button>
+      <button onClick={() => handleInput("-")}>-</button>
+      <br />
+      <button onClick={() => handleInput(1)}>1</button>
+      <button onClick={() => handleInput(2)}>2</button>
+      <button onClick={() => handleInput(3)}>3</button>
+      <button onClick={() => handleInput("*")}>*</button>
+      <br />
+      <button onClick={() => handleInput("c")}>c</button>
+      <button onClick={() => handleInput(0)}>0</button>
+      <button onClick={() => handleInput("=")}>=</button>
+      <button onClick={() => handleInput("/")}>/</button>
+    </>
+  );
+}
+
+export default Calculator;
